421703751d4a971b5f89eee94c4ea074350c33ab
“kazuki.oomori” <kazukiayk79@gmail.com>
2021-02-10 10:33:09 +0000

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
+  
 end
