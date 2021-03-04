201069ae35593522c6d3c775134e1638bf07dbf6
oomori-kazuki <kazukiayk79@gmail.com>
2021-03-04 07:44:23 +0000

    Add design to  users/users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email.downcase! }
   has_secure_password
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-  validates :profile, presence: true,length: { maximum: 255 }
+  validates :profile, presence: true,length: { maximum: 100 }
   mount_uploader :image, ImageUploader
   has_many :posts, dependent: :destroy
   belongs_to :general_user
