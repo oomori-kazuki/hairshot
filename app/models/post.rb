7cce77272d43f18ffb02a8b4abd8289ce42b9b65
oomori-kazuki <kazukiayk79@gmail.com>
2021-03-15 07:04:03 +0000

    Fix posts/new preview

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,6 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   
   validates :title, presence: true, length: { maximum: 15 } 
+  validates :image, presence: true
   mount_uploader :image, ImageUploader
 end
