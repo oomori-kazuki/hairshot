cbe8e2a3148c05f08f0f3561e0478ce42a758f7b
oomori-kazuki <kazukiayk79@gmail.com>
2021-03-16 05:37:24 +0000

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,17 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'], 
-      :aws_access_key_id     => ENV['AKIAQACFOZZR77JYN36N'],
-      :aws_secret_access_key => ENV['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']
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:AKIAQACFOZZR77JYN36N],
+      aws_secret_access_key: Rails.application.credentials.aws[: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],
+      region: 'ap-northeast-1'
     }
-    config.fog_directory     =  ENV['rails-avatar1']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
+    
+    config.fog_directory = 'rails-avatar1'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-avatar1'
   end
 
   # 日本語ファイル名の設定
